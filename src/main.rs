590c7e0d1d131a773af4827e37cc995c48d042cf
qpxdesign <qpxdesign@users.noreply.github.com>
2025-10-12 20:49:20 -0700

    change port

--- src/main.rs
+++ src/main.rs
@@ -81,6 +81,6 @@ async fn main() {
         .layer(CookieLayer::strict());
 
     // run our app with hyper, listening globally on port 3000
-    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:1946").await.unwrap();
     axum::serve(listener, app).await.unwrap();
 }
