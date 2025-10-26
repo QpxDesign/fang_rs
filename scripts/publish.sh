a3414101e99f5884f319f34a8d0a4fff98de5b76
qpxdesign <qpxdesign@users.noreply.github.com>
2025-10-26 13:51:21 -0700

    publish

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -1,4 +1,4 @@
 git add .
 git commit -m "publish"
 git push -u origin main
-ssh quinn@serverus "cd /home/quinn/projects/fang_rs; git fetch; git pull; docker build . -t fang; docker save fang > builds/fang.tar; scp builds/fang.tar root@qcloud-1:/root/docker/nginx/sites/fang_rs/builds"
+ssh quinn@serverus "cd /home/quinn/projects/fang_rs; git fetch; git pull; docker build . -t fang; docker save fang > builds/fang.tar; scp builds/fang.tar root@100.87.34.34:/root/docker/nginx/sites/fang_rs/builds"
